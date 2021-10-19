715b7bd3dedcf5ef8b62b425fbdf626a3f05a81e
francois joubert <fxa.joubert@gmail.com>
2021-10-19 14:29:52 +0200

    #1 - Add a package structure and a setup script.

---
+++ setup.py
@@ -0,0 +1,40 @@
+import pathlib
+
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+dev_deps = [
+    "flake8==4.0.1",
+    "mypy==0.910",
+    "pytest==6.2.5"
+]
+
+setup(
+    name="chowder_impl",
+    version="0.0.0",
+    description="Implementation of the CHOWDER model for histopathological image analysis.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joubs/chowder_impl",
+    author="Francois Joubert",
+    author_email="fxa.joubert@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["chowder"],
+    include_package_data=True,
+    install_requires=[],
+    extras_require={
+        "dev": dev_deps
+    },
+    entry_points={
+        "console_scripts": [
+            "chowder_train=chowder.__main__:main",
+        ]
+    }
+)
